0eaca278bd63a1aa89d74d83bc16d7dd22364ba8
Alex Orlov <a.orlov@pixelartsoft.com>
2026-02-10 17:20:36 +0300

    finish cors

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'https://sea-fight-sandy.vercel.app',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
